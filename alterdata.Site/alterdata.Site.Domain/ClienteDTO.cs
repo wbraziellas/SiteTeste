bdbd64d6fd13b7815d19d9a75e134dda401d139f
wbraziellas <wbraziellas@hotmail.com>
2016-12-15 19:52:55 +0000

    Inclusão dos repositórios para o CRUD na base.

--- alterdata.Site/alterdata.Site.Domain/ClienteDTO.cs
+++ alterdata.Site/alterdata.Site.Domain/ClienteDTO.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace alterdata.Site.Domain
 {
-    public class Class1
+    public class ClienteDTO
     {
+        public int id { get; set; }
+        public string nome { get; set; }
+        private string _cpf;
+        public string cpf
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                _cpf = value.Replace(".", "");
+                _cpf = _cpf.Replace("-", "");
+            }
+        }
+        public string rg { get; set; }
+        public string endereco { get; set; }
+        public string telefone { get; set; }         
     }
 }
---
+++ alterdata.Site/alterdata.site.Repository/ClienteDTORepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using alterdata.Site.Domain;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alterdata.site.Repository
+{
+    public class ClienteDTORepository
+    {
+        public string selectCostumers()
+        {
+            var comandoSql = "select * from clientes";
+            return comandoSql;
+        }
+
+        public string updateCostumer()
+        {
+            var comandoSql = "update clientes set id = @_id, nome = @_nome, cpf = @_cpf, rg = @_rg, endereco = @_endereco, telefone = @_telefone";
+            return comandoSql;
+        }
+
+        public string addCostumer(ClienteDTO cliente)
+        {
+            var comandoSql = "insert into clientes(id, nome, cpf, rg, endereco, telefone)values()";
+            return comandoSql;
+        }
+    }
+}
