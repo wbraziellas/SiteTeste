73a7e5a3d1bf0c1796d49db1bac0301f55d321f4
wbraziellas <wbraziellas@hotmail.com>
2016-12-16 14:58:37 +0000

    Criado método de conexão ao banco de dados.

---
+++ alterdata.Site/alterdata.Site.Controller/ConexaoPostgres.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+using alterdata.site.Repository;
+
+namespace alterdata.Site.Controller
+{
+    public class ConexaoPostgres
+    {
+        public DataTable ConectarBasePostGres(string comandoSql)
+        {
+            var stringConnection = "User ID=root;Password=myPassword;Host=localhost;Port=5432;Database=myDataBase;Pooling=true;Min Pool Size=0;Max Pool Size=100;Connection Lifetime=0";
+            
+            using (var conexao = new NpgsqlConnection(stringConnection))
+            {
+                try
+                {
+                    if (conexao.State == ConnectionState.Closed)
+                    {
+                        conexao.Open();
+                    }
+
+                    var sqlCommand = new NpgsqlCommand(comandoSql);
+                    var adapter = new NpgsqlDataAdapter() { SelectCommand = sqlCommand };
+                    var data = new DataTable();
+
+                    adapter.Fill(data);
+
+                    return data;
+                }
+                catch
+                {
+                    return new DataTable();
+                }
+                finally
+                {
+                    conexao.Close();
+                }
+            }            
+        }
+
+                
+    }
+}
--- alterdata.Site/alterdata.site.Repository/ClienteDTORepository.cs
+++ alterdata.Site/alterdata.site.Repository/ClienteDTORepository.cs
@@ -7,21 +7,21 @@ using System.Threading.Tasks;
 
 namespace alterdata.site.Repository
 {
-    public class ClienteDTORepository
+    public static class ClienteDTORepository
     {
-        public string selectCostumers()
+        public static string selectCostumers()
         {
             var comandoSql = "select * from clientes";
             return comandoSql;
         }
 
-        public string updateCostumer()
+        public static string updateCostumer()
         {
             var comandoSql = "update clientes set id = @_id, nome = @_nome, cpf = @_cpf, rg = @_rg, endereco = @_endereco, telefone = @_telefone";
             return comandoSql;
         }
 
-        public string addCostumer(ClienteDTO cliente)
+        public static string addCostumer(ClienteDTO cliente)
         {
             var comandoSql = "insert into clientes(id, nome, cpf, rg, endereco, telefone)values()";
             return comandoSql;
