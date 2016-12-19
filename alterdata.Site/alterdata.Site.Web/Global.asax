ee875c21ffdc3b8c2a934b0fa476366050ffab57
wbraziellas <wbraziellas@hotmail.com>
2016-12-19 19:52:20 +0000

    Inserido o serviço Web.

---
+++ alterdata.Site/alterdata.Site.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="alterdata.Site.Web.Global" Language="C#" %>
---
+++ alterdata.Site/alterdata.Site.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(alterdata.Site.Web.Startup))]
+namespace alterdata.Site.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
